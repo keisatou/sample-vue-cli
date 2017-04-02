1527ef914a89550ea8d3d84c4cc882ea5c1ea93e
keisatou <dummy>
2017-04-02 19:46:58 +0900

    first commit

---
+++ src/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <p>Server Stataus: {{ status }}</p>
+  <hr>
+  <button>
+    button
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      status: 'Critical'
+    }
+  }
+}
+</script>
+
+<style>
+</style>
